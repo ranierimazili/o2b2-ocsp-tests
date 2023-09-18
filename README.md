dfe6e3f7b104550b5334213bb99335f908abb207
Ranieri Mazili <ranieri85@gmail.com>
2023-09-18 11:55:29 -0300

    first version

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# o2b2-ocsp-tests
-Realiza o teste de OCSP das entidades certificadoras do Open Finance Brasil
+# The open Open Banking Brasil project - OCSP over HTTP tests
+Realiza o teste de OCSP das entidades certificadoras do Open Finance Brasil utilizando os verbos POST e GET
+
+## Como executar
+Após fazer download do código fonte, instale as dependências
+
+```
+npm install
+```
+
+Edite o arquivo index.js, substituindo as valores das varíavels CA e METHOD para refletirem o cenário de teste
+
+```
+const CA = 'SERPRO'; //CERTISIGN|SERASA|SERPRO|SOLUTI
+const METHOD = 'GET'; //GET|POST
+```
+
+No exemplo acima, será testado o OCSP via GET na AC Serpro.
+
+Para executar o teste, rode o comando abaixo:
+
+```
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,44 @@
+import { readFileSync } from 'fs';
+import ocsp from 'ocsp';
+
+const CA = 'SERPRO'; //CERTISIGN|SERASA|SERPRO|SOLUTI
+const METHOD = 'GET'; //GET|POST
+
+let certPath, issuerPath;
+switch (CA) {
+    case 'CERTISIGN':
+        certPath = 'certs/certisign_cert.cer';
+        issuerPath = 'certs/certisign_ca.cer';
+        break;
+    case 'SERASA':
+        certPath = 'certs/serasa_cert.cer';
+        issuerPath = 'certs/serasa_ca.cer';
+        break;
+    case 'SERPRO':
+        certPath = 'certs/serpro_cert.cer';
+        issuerPath = 'certs/serpro_ca.cer';
+        break;
+    case 'SOLUTI':
+        certPath = 'certs/soluti_cert.cer';
+        issuerPath = 'certs/soluti_ca.cer';
+        break;
+}
+
+let cert, issuer;
+try {
+    cert = readFileSync(certPath, 'utf8');
+    issuer = readFileSync(issuerPath, 'utf8');
+} catch (err) {
+    console.log(err);
+}
+
+ocsp.check({
+    cert: cert,
+    issuer: issuer,
+    requestByGet: (METHOD === 'GET')
+}, function(err, res) {
+    if (err)
+      console.log(err);
+
+    console.log(res);
+});
