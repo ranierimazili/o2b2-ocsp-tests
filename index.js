85d1ba32f7aeab6e20965360ab97274a83ab86ce
Ranieri Mazili <ranieri85@gmail.com>
2025-02-11 10:23:01 -0300

    Refatoração/simplificação

--- index.js
+++ index.js
@@ -4,25 +4,8 @@ import ocsp from 'ocsp';
 const CA = 'SERASA'; //CERTISIGN|SERASA|SERPRO|SOLUTI
 const METHOD = 'POST'; //GET|POST
 
-let certPath, issuerPath;
-switch (CA) {
-    case 'CERTISIGN':
-        certPath = 'certs/certisign_cert.cer';
-        issuerPath = 'certs/certisign_ca.cer';
-        break;
-    case 'SERASA':
-        certPath = 'certs/serasa_cert.cer';
-        issuerPath = 'certs/serasa_ca.cer';
-        break;
-    case 'SERPRO':
-        certPath = 'certs/serpro_cert.cer';
-        issuerPath = 'certs/serpro_ca.cer';
-        break;
-    case 'SOLUTI':
-        certPath = 'certs/soluti_cert.cer';
-        issuerPath = 'certs/soluti_ca.cer';
-        break;
-}
+const certPath = `certs/${CA.toLowerCase()}_cert.cer`;
+const issuerPath = `certs/${CA.toLowerCase()}_ca.cer`;
 
 let cert, issuer;
 try {
